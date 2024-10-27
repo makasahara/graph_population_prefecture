7cfcffefbf0f5063a2570ff73c488b2493ce5c8f
masashi.kasahara <ma.kasahara2369@gmail.com>
2024-10-27 14:49:37 +0900

    関数単体テストを作成

--- src/util/api/api.ts
+++ src/util/api/api.ts
@@ -4,8 +4,6 @@ import { resasApiBaseEndpoint } from "./define";
 
 const getPrefectures = async (): Promise<PrefectureType[]> => {
   const data = await getData(generateUrl("api/v1/prefectures"));
-  console.log("api/v1/prefectures"); // 実行時のログとして出力
-  console.log(data); // データをログとして出力
   return data.result;
 };
 
@@ -23,13 +21,14 @@ const getPopulation = async (prefCodes: string[]) => {
 };
 
 const generateUrl = (pathParameters: string, queryParameters: string = "") => {
-  return `${resasApiBaseEndpoint}${pathParameters}?${queryParameters}`;
+  const queryString = queryParameters ? `?${queryParameters}` : "";
+  return `${resasApiBaseEndpoint}${pathParameters}${queryString}`;
 };
 
 const getData = async (url: string) => {
   const response = await axios.get(url, {
     headers: {
-      "X-API-KEY": import.meta.env.VITE_RESAS_API_KEY,
+      "X-API-KEY": process.env.VITE_RESAS_API_KEY,
     },
   });
   return response.data;
