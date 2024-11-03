8616680c4dd9cd03e5b1386b4667a774a05f090c
masashi.kasahara <ma.kasahara2369@gmail.com>
2024-11-03 17:14:56 +0900

    topPgコンポーネントのテストを修正。

--- src/components/5-pg/topPg/index.test.tsx
+++ src/components/5-pg/topPg/index.test.tsx
@@ -3,7 +3,9 @@ import TopPg from "./index";
 
 const topTmplMockText = "TopTmpl Component";
 
-jest.mock("../../4-tmpl/topTmpl", () => <div>{topTmplMockText}</div>);
+jest.mock("../../4-tmpl/topTmpl", () => {
+  return jest.fn(() => <div>{topTmplMockText}</div>);
+});
 
 describe("TopPg", () => {
   it("TopTmplが表示されること", () => {
